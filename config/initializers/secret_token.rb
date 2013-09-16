16da83759cd52180ca324708ae4e4e17bd07b013
Gabriel Cebrian <gabceb@gmail.com>
2013-09-15 18:35:44 -0700

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bikeshare::Application.config.secret_key_base = 'a8167117492d8259185aa6c539cc1cd3954c94eb037652adc3ca8996971a7694645c46bbbe431eba3bc7db86a24e05b377ddee723ca85fb52580728dcbc4022d'
